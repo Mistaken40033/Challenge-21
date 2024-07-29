2ee0bb3bc652132f7de3f253dea397399db5c0a5
Erica Balderas <ericab40033@hotmail.com>
2024-07-28 18:10:16 -0600

    adding files

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+// src/utils/mutations.js
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
